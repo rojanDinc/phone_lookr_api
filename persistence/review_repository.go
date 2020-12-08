0f520c8d1a7f88ae03d442f4f68cbb9cb7f40491
Rojan Dinc <rojan.dinc@valtech.se>
2020-12-08 23:35:37 +0100

    Updated repository crud functions

--- persistence/review_repository.go
+++ persistence/review_repository.go
@@ -1,6 +1,9 @@
 package persistence
 
 import (
+	"errors"
+	"fmt"
+	app_error "phone_lookr/error"
 	"phone_lookr/model"
 	"sync"
 )
@@ -24,19 +27,35 @@ func NewSyncMapReviewRepository() ReviewRepository {
 }
 
 func (repo *reviewRepository) Add(r *model.Review) error {
+	if r == nil {
+		return app_error.RepositoryAddError{
+			InnerError: errors.New("The provided review is nil"),
+		}
+	}
 	repo.store.Store(r.TelephoneNumber, r)
-	return nil // Return error if database an error can be created
+	return nil
 }
 
 func (repo *reviewRepository) Find(telephoneNumber string) (*model.Review, error) {
 	review, ok := repo.store.Load(telephoneNumber)
 	if ok == false {
-		return nil, nil // Return error if database an error can be created
+		return nil, nil
 	}
 	return review.(*model.Review), nil
 }
 
 func (repo *reviewRepository) Remove(telephoneNumber string) error {
+	review, err := repo.Find(telephoneNumber)
+	if err != nil {
+		return app_error.RepositoryRemoveError{
+			InnerError: err,
+		}
+	}
+	if review == nil {
+		return app_error.RepositoryRemoveError{
+			InnerError: fmt.Errorf(`A review for the telephone number "%s" does not exist`, telephoneNumber),
+		}
+	}
 	repo.store.Delete(telephoneNumber)
-	return nil // Return error if database an error can be created
+	return nil
 }
